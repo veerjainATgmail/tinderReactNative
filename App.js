1bdc120ff0b286259c463541bcdada160693b70b
Ryan <rjlafazan@gmail.com>
2018-08-26 13:50:53 -0700

    navigation files created and react-navigation npm installed

--- App.js
+++ App.js
@@ -1,22 +1,14 @@
 import React from "react";
 import styles from "./styles.js";
+import Home from "./screens/Home.js";
 import { Text, View } from "react-native";
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Test Environment</Text>
+        <Home />
       </View>
     );
   }
 }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: "#fff",
-//     alignItems: "center",
-//     justifyContent: "center",
-//   },
-// });
