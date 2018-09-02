e20c3c8638a703c6021d22f721feeaac546bcdfb
Ryan <rjlafazan@gmail.com>
2018-09-01 18:04:09 -0700

    Facebook login modal integration with Home screen and start of firebase integration

--- screens/Home.js
+++ screens/Home.js
@@ -3,14 +3,47 @@ import styles from "../styles";
 import { connect } from "react-redux";
 import { login } from "../redux/actions";
 
-import { Text, View } from "react-native";
+import { Text, View, Alert } from "react-native";
 
 class Home extends React.Component {
   state = {};
 
   componentWillMount() {
     this.props.dispatch(login("logging in!"));
+    this.login();
   }
+
+  login = async () => {
+    const { type, token } = await Expo.Facebook.logInWithReadPermissionsAsync(
+      "2212989722278704",
+      {
+        permissions: ["public_profile"],
+      }
+    );
+    if (type === "success") {
+      // Get the user's name using Facebook's Graph API
+      const response = await fetch(
+        `https://graph.facebook.com/me?access_token=${token}`
+      );
+      Alert.alert("Logged in!", `Hi ${(await response.json()).name}!`);
+    }
+  };
+
+  // logIn = async () => {
+  //   const { type, token } = await Expo.Facebook.logInWithReadPermissionsAsync(
+  //     "2212989722278704",
+  //     {
+  //       permissions: ["public_profile"],
+  //     }
+  //   );
+  //   if (type === "success") {
+  //     // Get the user's name using Facebook's Graph API
+  //     const response = await fetch(
+  //       `https://graph.facebook.com/me?access_token=${token}`
+  //     );
+  //     Alert.alert("Logged in!", `Hi ${(await response.json()).name}!`);
+  //   }
+  // };
 
   render() {
     return (
